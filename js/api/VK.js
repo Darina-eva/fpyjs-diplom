4b8a5d2b6f592f7ea1029fd07ab33fc89a3fcffe
Darina-eva <darina.eva00.00@mail.ru>
2026-08-04 00:08:14 +0300

    feat: implement VK.get and VK.processData via JSONP

--- js/api/VK.js
+++ js/api/VK.js
@@ -6,14 +6,39 @@
  * */
 class VK {
 
-  static ACCESS_TOKEN = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008';
   static lastCallback;
+
+  static get ACCESS_TOKEN() {
+    let token = localStorage.getItem('vkToken');
+
+    if (!token) {
+      token = prompt('Введите токен доступа для VK API');
+      localStorage.setItem('vkToken', token);
+    }
+
+    return token;
+  }
 
   /**
    * Получает изображения
    * */
   static get(id = '', callback){
+    this.lastCallback = callback;
 
+    const params = new URLSearchParams({
+      owner_id: id,
+      album_id: 'profile',
+      extended: 1,
+      access_token: this.ACCESS_TOKEN,
+      v: '5.131',
+      callback: 'VK.processData',
+    });
+
+    const script = document.createElement('script');
+    script.src = `https://api.vk.com/method/photos.get?${params}`;
+    script.id = 'vk-request';
+
+    document.body.appendChild(script);
   }
 
   /**
@@ -21,6 +46,22 @@ class VK {
    * Является обработчиком ответа от сервера.
    */
   static processData(result){
+    const script = document.getElementById('vk-request');
+    if (script) {
+      script.remove();
+    }
 
+    if (result.error) {
+      alert(result.error.error_msg);
+      return;
+    }
+
+    const images = result.response.items.map(item => {
+      const largest = item.sizes.reduce((max, size) => size.width > max.width ? size : max);
+      return largest.url;
+    });
+
+    this.lastCallback(images);
+    this.lastCallback = () => {};
   }
 }
